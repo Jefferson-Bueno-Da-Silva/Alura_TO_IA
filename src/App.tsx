65dc055f60b8cc1601b10f025d42eec7ae49254b
jefferson bueno <jeffersonbuenoprofissional@gmail.com>
2024-05-10 10:54:48 -0300

    feat: add markdown render

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,59 @@ import { FormEvent, useState } from 'react';
 import './App.css'
 import {
   GoogleGenerativeAI,
+  HarmBlockThreshold,
+  HarmCategory,
 } from "@google/generative-ai"
 import { Box, Button, CircularProgress, FormControl, Link, TextField, Typography } from '@mui/material';
+import Markdown from 'react-markdown'
 import { DICTIONARY, SYSTEM_INSTRUCTION } from './constants/dictionary';
 
 
 const MODEL_NAME = "gemini-1.5-pro-latest";
-const API_KEY = "<your_api_key>";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
   const genAI = new GoogleGenerativeAI(API_KEY);
   const [clinicalCase, setClinicalCase] = useState<string>()
-  const [result, setResult] = useState('<h1>Aguardando caso clinico...</h1>')
+  const [result, setResult] = useState('# Aguardando caso clinico...')
   const [loading, setLoading] = useState(false)
 
   async function onSubmit(e?: FormEvent<HTMLFormElement>) {
     e?.preventDefault()
     setLoading(true)
-    const prompt = SYSTEM_INSTRUCTION + DICTIONARY.toString() + "use html para formatação"
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME, systemInstruction: prompt });
+    const prompt = SYSTEM_INSTRUCTION + DICTIONARY.toString() + "formate usando markdown"
+    const model = genAI.getGenerativeModel({
+      model: MODEL_NAME, systemInstruction: prompt,
+      generationConfig: {
+        candidateCount: 1,
+        temperature: 0.5
+      },
+      safetySettings: [
+        {
+          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+          threshold: HarmBlockThreshold.BLOCK_NONE
+        },
+        {
+          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
+        },
+      ],
+    });
 
     if (clinicalCase) {
-      const response = await model.generateContent(clinicalCase)
+      const response = await model.generateContent(clinicalCase).finally(() => {
+        setLoading(false)
+      })
       setResult(response.response.text())
     }
-    setLoading(false)
   }
 
 
@@ -53,12 +81,14 @@ function App() {
           <Button sx={{ marginTop: '1rem' }} disabled={loading || !clinicalCase} type='submit' variant='contained'>Envie seu caso</Button>
         </FormControl>
       </Box>
-      <Box marginTop={10} component={"section"} minHeight={300} display={"flex"} flexDirection={"column"} alignItems={"center"}>
-        <Typography variant='h6' marginBottom={2}>Resultado: </Typography>
+      <Box marginTop={10} component={"section"} minHeight={300} display={"flex"} flexDirection={"column"} alignItems={"start"}>
+        <Typography alignSelf={"center"} variant='h6' marginBottom={2}>Resultado: </Typography>
         {loading ? (
-          <CircularProgress sx={{ marginTop: "2rem" }} color="secondary" />
+          <CircularProgress sx={{ marginTop: "2rem", alignSelf: "center" }} color="secondary" />
         ) : (
-          <div dangerouslySetInnerHTML={{ __html: result }} />
+          <Typography textAlign={"start"}>
+            <Markdown>{result}</Markdown>
+          </Typography>
         )}
       </Box>
 
@@ -67,6 +97,8 @@ function App() {
         Seja detalhista, forneça o máximo de informações possíveis sobre seu/sua filho(a),
         use o exemplo no final da pagina para visualizar a saída desejada.
       </Typography>
+      <br />
+      <Typography>Gemini: "Ao descrever uma pessoa, lugar ou coisa, não se limite a listar adjetivos. Mergulhe nos detalhes sensoriais! Em vez de dizer que um personagem tem "olhos bonitos", descreva a cor, a forma e a expressão deles"</Typography>
 
       <Box component={"section"} marginTop={8}>
         <Typography variant='h6' marginBottom={1}>Exemplo:</Typography>
